d0e8bd017a85d10bf1e0fb37b1dd4e665afd610b
AhmadWaleedAlmustafa <ahamdalmostafa0099@gmail.com>
2025-07-18 20:40:35 +0300

    using express ti listen

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+ const express =require('express');
+ const app =express();
+ app.get('/',(req,res,next)=> {
+    res.json({'message':'ok'})
+ });
+  app.get('/test',(req,res,next)=> {
+    res.json({'message':'ok'})
+ });
+   app.get('/test1',(req,res,next)=> {
+    res.json({'message':'ok'})
+ });
  
+ app.listen(3000,()=>console.log('listening on port 3000'))
